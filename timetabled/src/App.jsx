5ac29fe85068e75b8908a5b1cfc56667c6635d6b
David R. <dreyes@colgate.edu>
2025-09-24 21:51:16 -0400

    initial commit

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from '../components/Calendar';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Itinerary for 7 days in Korea</h1>
+      <h2>Welcome to Korea David! Look at ThIs CAleNDaR</h2>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
